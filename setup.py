1586e71c1478c4b0a513734aa30edd2d617c4877
Peter <peter.williams@yale.edu>
2023-12-04 20:40:18 -0500

    adding cookiecutter directory organization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repository to mine and curate dataset and build a model to predict mlb games against the betting spread',
+    author='peter',
+    license='MIT',
+)
